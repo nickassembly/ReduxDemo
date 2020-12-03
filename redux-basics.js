2863271c13c85ef0c529404ef3b1d534851f33b8
nickguerra <nickguerra@gmail.com>
2020-12-03 07:19:33 -0600

    connected store and actions for redux

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ counter: 0
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {       
+        return {
+         ...state,
+         counter: state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {       
+        return {
+         ...state,
+         counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -14,8 +26,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Reducer
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
-// Subscription
