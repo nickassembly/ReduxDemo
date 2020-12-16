265eba9d8527c297fb4496b8b8c6a543925b8cf7
nickguerra <nickguerra@gmail.com>
2020-12-15 18:22:11 -0600

    refactored reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,23 +1,22 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility.js'
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch(action.type) {
      case actionTypes.STORE_RESULT:
-         // Change data
-         return {
-             ...state,
-             results: state.results.concat({id: new Date(), value: action.result})
-         }
+         return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
+ 
      case actionTypes.DELETE_RESULT:
-        const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-         return {
-            ...state,
-            results: updatedArray
-         };
+       return deleteResult(state, action); 
     }
 
     return state;
